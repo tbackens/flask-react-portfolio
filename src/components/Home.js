7c9302997bf0745a67e5d868dcfe9c0857ad66d3
tbackens <99706186+tbackens@users.noreply.github.com>
2023-03-20 10:18:34 +0100

    added components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import timo from './timo.jpg'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div class="container">
+      <div class="row justify-content-evenly">
+        <div class="col">
+          <img src={timo} class="img-thumbnail rounded-circle w-50 h-auto m-3" alt="..."/>
+        </div>
+      </div>
+      <div class="row justify-content-evenly">
+        <div class="col">
+          <div class="card bg-dark">
+            <div class="card-body">
+              <h4>Hi, I'm Timo!</h4>
+              <h5>I'm a Fullstack-Developer</h5>
+              <ul class="text-start">
+                <li class="">Flensburg, Germany</li>
+                <li>Student at Codecademy</li>
+                <li>t.backens@web.de</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
