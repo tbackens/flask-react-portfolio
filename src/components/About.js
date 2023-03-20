ceee01a5fbd930cc8c2ea6f50ea868062c33204c
tbackens <99706186+tbackens@users.noreply.github.com>
2023-03-20 17:30:14 +0100

    added about section

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,47 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='Container'>About</div>
+    <div className='container'>
+      <div class="card bg-dark mt-3">
+        <div class="card-header">
+          <h4>About Me</h4>
+        </div>
+        <div class="card-body text-start">
+          <p>
+            I'm Timo! Before I started coding, I studied Marine Engineering,
+            worked as an officer on containerships and as an Operation Supervisor in a Power Plant.
+            Starting as a hobby, coding gives me now a new lkvjhefvpv dcvqsdövkledhjqvqde ölksdjvhqdev,
+            wdfdf wdfowdjkfh. Dfierughergddff. DFfd fwd.
+          </p>
+        </div>
+      </div>
+      <div class="card bg-dark mt-4">
+        <div class="card-header">
+          <h4>Skills</h4>
+        </div>
+        <div class="card-body text-center">
+          <ul class="list-unstyled text-center">
+            <li><h5>Languages</h5></li>
+            <li>Python</li>
+            <li>JavaScript</li>
+            <li>HTML/CSS</li>
+            <br/>
+            <li><h5>Frameworks</h5></li>
+            <li>React</li>
+            <li>Flask</li>
+            <li>Bootstrap</li>
+            <li>PyQt</li>
+            <br/>
+            <li><h5>Databases</h5></li>
+            <li>MySQL</li>
+            <br/>
+            <li><h5>Tools</h5></li>
+            <li>Git/ Github</li>
+            <li>Postman</li>
+          </ul>
+        </div>
+      </div>
+    </div>
   )
 }
 
