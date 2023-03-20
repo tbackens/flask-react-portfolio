1a220939f81ec34a5453176fb48877bfe099f8d2
tbackens <99706186+tbackens@users.noreply.github.com>
2023-03-20 18:29:42 +0100

    footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='nav justify-content-center mt-3 text-secondary' id='footer'>
+        <footer>
+            <p>© 2023 Timo Backens</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
